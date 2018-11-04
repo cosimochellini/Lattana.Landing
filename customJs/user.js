f68a28dfd4705590a2db0ccf336a108599da3ff6
cosimo chellini <iMac@iMac.local>
2018-11-04 19:39:57 +0100

    added class user

--- customJs/user.js
+++ customJs/user.js
@@ -1,10 +1,40 @@
-class userController {
-    get user() {
-        return this._getUser();
+class user {
+    constructor() {
+        let currentUser = this._getUser();
+        if (!currentUser) {
+            this.logged = false;
+            console.log("Nessun Utente");
+            return;
+        }
+        this.token = currentUser.token.access_token;
+        this.roles = currentUser.app_metadata.roles;
+        this.email = currentUser.email;
+        this.logged = true;
+
     }
 
-    _getUser(){
-
+    _getUser() {
+        try {
+            return JSON.parse(localStorage["gotrue.user"]);
+        } catch (ex) {
+            return null;
+        }
     }
-    
+
+    static get Type() {
+        return {
+            "Admin": "Admin",
+            "User": "User",
+            "Collaborator": "Collaborator",
+            "Other" : "Other"
+        }
+    }
+    /**
+     * return if the user is a specific role
+     * @param {string} role 
+     */
+    is(role) {
+        return this.roles.map(role => role.toLocaleLowerCase()).contains(role.toLocaleLowerCase());
+    }
+
 };
