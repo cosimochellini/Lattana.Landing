c6db96fffe773e6df4654b1f0708cabb792e7dfe
c.chellini <c.chellini@inlinea.net>
2018-11-05 11:49:23 +0100

    use of global variables

--- functions/hello.js
+++ functions/hello.js
@@ -2,6 +2,6 @@ exports.handler = function(event, context, callback) {
     console.log(arguments);
     callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: process.env.mongoPwd
     });
 }
