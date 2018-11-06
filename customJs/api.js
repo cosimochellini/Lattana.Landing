3d1f86926017671da7c6ad19e325563ab2dca027
Cosimo <cosimo.chellini@gmail.com>
2018-11-06 17:22:38 +0100

    added api class

---
+++ customJs/api.js
@@ -0,0 +1,36 @@
+function postData(url = ``, data = {}) {
+    // Default options are marked with *
+    return fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, cors, *same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            // "Content-Type": "application/x-www-form-urlencoded",
+        },
+        redirect: "follow", // manual, *follow, error
+        referrer: "no-referrer", // no-referrer, *client
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+        .then(response => response.json()); // parses response to JSON
+}
+
+class Api {
+    constructor(url = '', data = {}, method = 'post') {
+        this.url = url;
+        this.settings = {
+            method: method,
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                "Authorization": `Bearer${new User().token}`
+            },
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+
+        }
+    }
+
+    run() {
+        return fetch(url, this.settings).then(response.json());
+    }
+}
