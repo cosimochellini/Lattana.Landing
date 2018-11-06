3659f23f221daddc4a6e0def78907d411002e611
c.chellini <c.chellini@inlinea.net>
2018-11-06 13:36:47 +0100

    pck.lock.json

--- functions/reserve.js
+++ functions/reserve.js
@@ -1,6 +1,12 @@
+const MongoClient = require('mongodb').MongoClient;
+
+
+
 exports.handler = function (event, context, callback) {
     console.log(arguments);
-    
+    if(MongoClient){
+        console.log("yeah");
+    }
     callback(null, {
         statusCode: 200,
         body: process.env.mongoPwd
