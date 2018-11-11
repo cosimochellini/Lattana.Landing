d494f9ea9b7c97afac349a76e409c3c0640870ef
Cosimo <cosimo.chellini@gmail.com>
2018-11-11 20:10:56 +0100

    test

--- customJs/api.js
+++ customJs/api.js
@@ -1,5 +1,5 @@
 const Api = axios.create({
-    baseURL: `${window.location.origin}/.netlify/functions/`,
+    // baseURL: `${window.location.origin}/.netlify/functions/`,
     baseURL: `localhost:9000/`,
     headers: {
         "Content-Type": "application/json; charset=utf-8",
