6b36d55716984cb41fef7d7a648fb177aa419f77
Cosimo <cosimo.chellini@gmail.com>
2018-11-13 11:40:54 +0100

    test identity

--- functions/prenotazioni.js
+++ functions/prenotazioni.js
@@ -2,96 +2,47 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
-import userClass from "../customJs/userClass";
+import user from "../customJs/userClass";
 
 const prenotazioni = require('../schema/prenotazioni');
 
 const prenotazioneCibo = require('../schema/prenotazioneCibo');
 
+const prenotazioniContext = mongoose.model('prenotazioni', prenotazioni, 'prenotazioni');
 
-const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Headers": "Content-Type"
-};
-
+const prenotazioneCiboContext = mongoose.model('prenotazioneCibo', prenotazioneCibo, 'prenotazioneCibo');
 
 exports.handler = async (event, context) => {
 
-    // if (!context && !context.clientContext) {
-    //     callback(null, {
-    //         statusCode: 401,
-    //         body: JSON.stringify(posts)
-    //     });
-    // }
+    const identity, currentUser;
 
-    // const { identity, user } = context.  clientContext;
+    if (!context && !context.clientContext) {
+        return {
+            statusCode: 401,
+            body: JSON.stringify(posts)
+        };
+    }
+
+    identity = context.clientContext.identity;
+
+    currentUser = new user(context.clientContext.user);
+
+    console.log(identity, user);
+
     const options = {
         reconnectTries: 100, // Never stop trying to reconnect
         reconnectInterval: 500, // Reconnect every 500ms
         poolSize: 10, // Maintain up to 10 socket connections
-        bufferMaxEntries: 0,
-        // useNewUrlParser: true
+        bufferMaxEntries: 0
     };
 
     mongoose.connect(process.env.db, options).then(success => console.log('db connesso')).catch(err => console.log(err));
 
-    // console.log('db connesso');
-
-    const prenotazioniContext = mongoose.model('prenotazioni', prenotazioni, 'prenotazioni');
-
-    console.log('mongoose,model');
-
-    const prenotazioneCiboContext = mongoose.model('prenotazioneCibo', prenotazioneCibo, 'prenotazioneCibo');
-
-    console.log('mongoose,model');
-
     const prenotazioniQuery = await prenotazioniContext.find({}).exec();
-
-    console.log(prenotazioniQuery);
 
     return {
         statusCode: 200,
         body: JSON.stringify(prenotazioniQuery)
     };
-    
-    // prenotazioneCiboContext.findOne(function (err, users) {
 
-    //     callback(null, {
-    //         statusCode: 200,
-    //         body: JSON.stringify(users)
-    //     });
-    // });
-    // console.log(query);
-    // console.log(arguments);
-
-    // const currentUser = new userClass(user);
-
-    //console.log("logged", currentUser.logged);
-
-    //console.log("event", event);
-
-    //console.log('context', context);
-
-    //console.log('identity ', identity);
-
-    // const body = event.body;
-
-    //console.log('body', body);
-
-    //const queryP = event.queryStringParameters;
-
-    //console.log('queryP', queryP);
-
-
-    //console.log(identity, user);
-
-    // if (mongoose) {
-    //     console.log("yeah");
-    // }
-    // callback(null, {
-    //     statusCode: 200,
-    //     body: JSON.stringify(query)
-    // });
 };
-
-// handler();
