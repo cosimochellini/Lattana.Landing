5da192e7a962062183f68922f644eb6e99686777
Cosimo <cosimo.chellini@gmail.com>
2018-11-15 16:51:29 +0100

    .-.

--- functions/prenotazioni.js
+++ functions/prenotazioni.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 import immportData from "../@api/core.js"
 
-import reservePanuozzoToday from "../@api/controller/prenotazioni"
+// import reservePanuozzoToday from "../@api/controller/prenotazioni"
 
 exports.handler = async (event, context) => {
 
@@ -17,7 +17,8 @@ exports.handler = async (event, context) => {
 
     switch (data.action) {
         case 'reservePanuozzoToday':
-            return reservePanuozzoToday(data);
+            console.log('entro in reservePanuozzoToday')
+            return _reservePanuozzoToday(data);
         default:
             console.log('invalid action => ', data.action);
             break;
@@ -31,3 +32,34 @@ exports.handler = async (event, context) => {
     // };
 
 };
+
+const _reservePanuozzoToday = ({ identity, currentUser, body, authorized, db }) => {
+
+    // if (!currentUser.logged) {
+    //     console.log('unathorized', currentUser);
+    //     return "unauthorized";
+    // }
+
+    // const prenotazioniCtx = db.prenotazioni;
+    // const prenotazioniCiboCtx = db.prenotazioniCibo;
+    // console.log('body', JSON.parse(body));
+
+    console.log('typeof body', typeof body);
+
+    let prenotazione = new db.prenotazioneCibo({
+        food: body.cibo,
+        username: body.username,
+        email: body.email,
+        date: new Date(),
+        prenotazioneId: '5be1b549e7179a6bbb967c21'
+    });
+
+    // console.log(prenotazione);
+
+    prenotazione.save(function (err) {
+        console.log('err prenotazione.save', err);
+    });
+
+    return 'ok';
+
+}
