490fe77a75d51bbf4ee97c1548110753a788fd01
Cosimo <cosimo.chellini@gmail.com>
2018-11-16 12:03:13 +0100

    modifiche al codice

--- @api/controller/prenotazioni.js
+++ @api/controller/prenotazioni.js
@@ -1,1 +1,22 @@
 
+const reservePanuozzoToday = async ({ identity, currentUser, body, authorized, db, callback }) => {
+
+    let prenotazione = new db.prenotazioneCibo({
+        food: body.cibo,
+        username: body.username,
+        email: body.email,
+        date: new Date(),
+        prenotazioneId: '5be1b549e7179a6bbb967c21'
+    });
+
+    // console.log(prenotazione);
+    try {
+        await prenotazione.save();
+    } catch (ex) {
+        console.log('err prenotazione.save', ex);
+    }
+
+};
+
+
+export default reservePanuozzoToday;
--- functions/prenotazioni.js
+++ functions/prenotazioni.js
@@ -1,23 +1,21 @@
 require('dotenv').config();
 
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 import { importData, _response } from "../@api/core.js"
 
-// import reservePanuozzoToday from "../@api/controller/prenotazioni"
+import reservePanuozzoToday from "../@api/controller/prenotazioni"
 
 exports.handler = async function (event, context) {
 
-    //    const { identity, currentUser, body, parameters, authorized, db, action } = immportData(event, context);
     const data = importData(event, context);
-
 
     console.log('action', data.action);
 
     switch (data.action) {
         case 'reservePanuozzoToday':
             console.log('entro in reservePanuozzoToday');
-            await _reservePanuozzoToday(data);
+            await reservePanuozzoToday(data);
             break;
         default:
             console.log('invalid action => ', data.action);
@@ -28,41 +26,5 @@ exports.handler = async function (event, context) {
     return _response(200, "tutto ok");
 
     // const prenotazioniQuery = await db.prenotazioni.find({}).exec();
-
-};
-
-const _reservePanuozzoToday = async ({ identity, currentUser, body, authorized, db, callback }) => {
-
-    // if (!currentUser.logged) {
-    //     console.log('unathorized', currentUser);
-    //     return "unauthorized";
-    // }
-
-    // const prenotazioniCtx = db.prenotazioni;
-    // const prenotazioniCiboCtx = db.prenotazioniCibo;
-    // console.log('body', JSON.parse(body));
-
-    //console.log('typeof body', typeof body);
-
-    let prenotazione = new db.prenotazioneCibo({
-        food: body.cibo,
-        username: body.username,
-        email: body.email,
-        date: new Date(),
-        prenotazioneId: '5be1b549e7179a6bbb967c21'
-    });
-
-    // console.log(prenotazione);
-    try {
-        await prenotazione.save();
-    } catch (ex) {
-        console.log('err prenotazione.save', ex);
-        //        callback(ex);
-    }
-
-    //  callback(null, {
-    //     statusCode: 200,
-    //   body: 'creato correttamente',
-    //});
 
 };
