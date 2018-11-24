5a90f8651e8c8d2284ed26e85fa7de5643d12160
Cosimo <cosimo.chellini@gmail.com>
2018-11-24 16:56:21 +0100

    modifiche al salvataggio delle prenotazioni

--- js/modules/@api/controller/prenotazioni.js
+++ js/modules/@api/controller/prenotazioni.js
@@ -1,5 +1,7 @@
 import {Model} from "mongoose";
 
+import {generateStartEnd} from "../../utils/date";
+
 /**
  * prenota un panuozzo per oggi, creando anche la prenotazione
  * @param {Object} param0 l'oggetto data generato da importData(...)
@@ -15,34 +17,60 @@ import {Model} from "mongoose";
  */
 const reservePanuozzoToday = async ({identity, currentUser, body, authorized, db}) => {
 
-    let prenotazione = new db.prenotazioni({
+    const [dataInizio, dataFine] = generateStartEnd();
+
+    //controllo se c'è già una prenotazione di quell
+    //utente per ogggi
+    let prenotazione = db.prenotazioni.find({
+        date: {$gte: dataInizio, $lt: dataFine},
         username: currentUser.username,
-        email: currentUser.email,
-        date: new Date()
+        email: currentUser.email
     });
+    console.log('prenotazione già esistente : ', prenotazione);
+
+    let prenotazioneCibo = db.prenotazioneCibo.find({
+        date: {$gte: dataInizio, $lt: dataFine},
+        username: currentUser.username,
+        email: currentUser.email
+    });
+    console.log('prenotazioneCibo già esistente : ', prenotazioneCibo);
+
 
     try {
-        await prenotazione.save();
-        let prenotazioneCibo = new db.prenotazioneCibo({
-            food: body.cibo,
-            username: currentUser.username,
-            email: currentUser.email,
-            date: new Date(),
-            text: body.note,
-            prenotazioneId: prenotazione.id
-        });
-        try {
-            await prenotazioneCibo.save();
-            return prenotazioneCibo;
-        } catch (ex) {
-            console.log('err prenotazioneCibo.save', ex);
-            return false;
+        if (!prenotazione) {
+            console.log('prenotazione non trovata');
+            prenotazione = new db.prenotazioni({
+                username: currentUser.username,
+                email: currentUser.email,
+                date: new Date()
+            });
+            await prenotazione.save();
         }
+        if (!prenotazioneCibo) {
+            console.log('prenotazioneCibo non trovata');
+
+            prenotazioneCibo = new db.prenotazioneCibo({
+                food: body.cibo,
+                username: currentUser.username,
+                email: currentUser.email,
+                date: new Date(),
+                text: body.note,
+                prenotazioneId: prenotazione.id
+            });
+            try {
+                await prenotazioneCibo.save();
+                return prenotazioneCibo;
+            } catch (ex) {
+                console.log('err prenotazioneCibo.save', ex);
+                return false;
+            }
+        }
+
     } catch (e) {
         console.log('err prenotazione.save', e);
         return false;
     }
-    return false;
+
 };
 
 export {
