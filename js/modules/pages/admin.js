fc9836b6acd92a5aa245addd932af9bc8cfcfbc7
Cosimo <cosimo.chellini@gmail.com>
2018-11-26 16:30:53 +0100

    aggiunte funzionalità alla pagina admin

--- js/modules/pages/admin.js
+++ js/modules/pages/admin.js
@@ -2,7 +2,7 @@ Vue.use(bootstrapVue);
 
 new Vue({
     el: '#app',
-    mixins : [mixin.mixin()],
+    mixins: [mixin.mixin()],
     data:
         {
             user: new User(),
@@ -12,16 +12,21 @@ new Vue({
             },
             items: {
                 cibo: [],
-                panuozzo: []
+                panuozzo: [],
+            },
+            commensali: {
+                items: [],
+                form: {
+                    selezione: 2
+                }
             },
             selezione: 'cibo',
-            fieldException : ['_id', 'prenotazioneId', '__v']
+            fieldException: ['_id', 'prenotazioneId', '__v']
         },
     mounted() {
         if (!this.user.logged) {
             // window.location.href = "./";
         }
-
         this.fetchData();
     },
     methods: {
@@ -42,11 +47,24 @@ new Vue({
                 query: {date: {"$gte": dataInizio, "$lt": dataFine}},
                 table: "prenotazioni"
             }).then((response) => this.items.panuozzo = response.data);
+        },
+        showCommensali() {
+            this.commensali.items = _bindCommensali(this.items.cibo, food);
+
+            this.$refs.modalCommensali.show()
         }
     },
     computed: {
-        computedItems(){
+        computedItems() {
             return this.items[this.selezione];
+        },
+        commensaliList() {
+            let commensali = [];
+            this.commensali.items.forEach(item => {
+                const selezione = this.commensali.form.selezione;
+                commensali.push(item);
+            });
+            return commensali;
         }
     },
     watch: {
@@ -60,3 +78,18 @@ new Vue({
 });
 
 window.netlifyIdentity.on("logout", () => window.location.href = "./");
+
+const food = [
+    {name: 'panuozzo', type: 0, price: 4},
+    {name: 'pizza', type: 0, price: 6},
+    {name: 'kebab', type: 1, price: 6},
+    {name: 'other', type: 1, price: 0}];
+
+const _bindCommensali = (items = [], food = []) => {
+    let commensali = [];
+    items.forEach(item => {
+        const _food = food.find(cibo => cibo.name === item.food);
+        commensali.push({...item, ..._food});
+    });
+    return commensali;
+};
