dcded610cbd6709ef98c28dae0167f9a06e1d5ba
Cosimo <cosimo.chellini@gmail.com>
2018-11-26 12:55:21 +0100

    modifiche pagina utente

--- js/modules/pages/user.js
+++ js/modules/pages/user.js
@@ -1,29 +1,39 @@
-let currentUser = new User();
-
 Vue.use(bootstrapVue);
 
 let vm = new Vue({
     el: '#app',
-    data() {
-        return {
-            user: currentUser,
-            foo : 'bar'
-        }
+    data: {
+        user: new User(),
+        prenotazioniPanuozzo: [],
+        prenotazioniCibo: []
     },
     mounted() {
         if (!this.user.logged) {
             // window.location.href = "./";
         }
+        this.getAllPrenotazioni();
     },
     methods: {
-        getUserRoles(){
-            if (!this.user.roles.length){
+        getUserRoles() {
+            if (!this.user.roles.length) {
                 return 'user';
             }
 
             return this.user.roles.join(',');
+        },
+        getAllPrenotazioni() {
+            Api('data').post('find', {
+                table: "prenotazioneCibo",
+                username: this.user.username
+            }).then(response => this.prenotazioniCibo = response.data);
+
+            Api('data').post('find', {
+                table: "prenotazioni",
+                username: this.user.username
+            }).then(response => this.prenotazioniPanuozzo = response.data);
         }
     }
+
 });
 
 window.netlifyIdentity.on("logout", () => window.location.href = "./");
