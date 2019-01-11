fccc5f0f9ddc0525123992c1e373d78be64c70fd
Cosimo <cosimo.chellini@gmail.com>
2019-01-11 18:19:58 +0100

    aggiunta burrata

--- js/modules/utils/food.js
+++ js/modules/utils/food.js
@@ -3,6 +3,8 @@ window.foodGlobal = [
     {name: 'panuozzo intero', type: 0, price: 8},
     {name: 'pizza margherita', type: 0, price: 6},
     {name: 'pizza nduja', type: 0, price: 7},
+    {name: 'pizza burrata', type: 0, price: 9},
     {name: 'kebab', type: 1, price: 6},
     {name: 'cibo da casa', type: 1, price: 0},
-    {name: 'other', type: 1, price: 0}];
+    {name: 'other', type: 1, price: 0}
+    ];
