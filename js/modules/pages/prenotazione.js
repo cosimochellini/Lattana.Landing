005c6acba32e330084383d332755f7a5871ce126
Cosimo <cosimo.chellini@gmail.com>
2019-01-19 12:54:58 +0100

    modifiche grafiche

--- js/modules/pages/prenotazione.js
+++ js/modules/pages/prenotazione.js
@@ -1,13 +1,97 @@
 let vm = new Vue({
     el: '#app',
     data: {
+        prenotazioni: [],
+        foods: window.foodGlobal,
         activeItem: 0
     },
     mounted() {
-        this.$vs.notify({
-            title: 'Connessione Effettuata',
-            color: 'primary',
-            icon: 'check_box'
-        });
+        this.fetchData({onMounted: true});
+
+    },
+    methods: {
+        fetchData(params = {}) {
+
+            Api('data').post('find', {
+                table: "prenotazioneCibo"
+            }).then((response) => {
+                if (params.onMounted) {
+                    this.$vs.notify({
+                        title: 'Connessione Effettuata',
+                        color: 'primary',
+                        icon: 'check_box'
+                    });
+                }
+                this.prenotazioni = response.data
+            });
+        },
+        bindCommensali() {
+            let items = this.prenotazioni || [];
+
+            let commensali = [];
+
+            items.forEach(item => {
+                const _food = this.foods.find(cibo => cibo.name === item.food);
+                commensali.push({...item, ..._food});
+            });
+            const pani = commensali.filter(c => c.food === 'mezzo panuozzo');
+
+            if (this.isOdd(pani.length)) {
+                const panoIndex = commensali.findIndex(c => c._id === pani[0]._id);
+                commensali[panoIndex] = {...commensali[panoIndex], price: 5, only: true};
+            }
+
+            return commensali;
+        },
+        isOdd(number) {
+            return number % 2;
+        }
+    },
+    computed: {
+        ordine() {
+            let ordine = [];
+
+            let commensali = this.bindCommensali();
+
+            commensali = commensali.filter(item => item.type === 0);
+
+            const cibiDaOrdinare = [...new Set(commensali.map(cibo => cibo.name))];
+
+            cibiDaOrdinare.forEach(cibo => {
+                if (cibo !== 'mezzo panuozzo') {
+                    ordine.push({
+                        name: cibo,
+                        quantity: commensali.filter(i => i.food === cibo).length,
+                        color: this.foods.find(x => x.name === cibo).color
+                    });
+                }
+            });
+            const mezzoPanuozzoQuantity = parseInt(commensali.filter(c => c.food === 'mezzo panuozzo').length);
+
+            if (this.isOdd(mezzoPanuozzoQuantity)) {
+                ordine.push({name: 'mezzo panuozzo', quantity: 1, color: '#006978'})
+            }
+
+            if (mezzoPanuozzoQuantity > 1) {
+                ordine.push({
+                    name: 'panuozzo 8 pezzi',
+                    quantity: parseInt(mezzoPanuozzoQuantity / 2),
+                    color : '#8e0000'
+                });
+            }
+
+            ordine = ordine.sort((a, b) => {
+                if (a.name < b.name) return -1;
+                if (a.name > b.name) return 1;
+                return 0;
+            });
+
+            return ordine;
+        }
     }
 });
+
+
+setInterval(() => {
+    vm.fetchData({onTimerUpdate: true});
+}, 10000);
--- js/modules/utils/food.js
+++ js/modules/utils/food.js
@@ -1,10 +1,10 @@
 window.foodGlobal = [
-    {name: 'mezzo panuozzo', type: 0, price: 4},
-    {name: 'panuozzo intero', type: 0, price: 8},
-    {name: 'pizza margherita', type: 0, price: 6},
-    {name: 'pizza nduja', type: 0, price: 7},
-    {name: 'pizza burrata', type: 0, price: 9},
-    {name: 'pizza margherita + salsiccia', type: 0, price: 7},
-    {name: 'kebab', type: 1, price: 6},
+    {name: 'mezzo panuozzo', type: 0, price: 4, color: '#006978'},
+    {name: 'panuozzo intero', type: 0, price: 8, color: '#001970'},
+    {name: 'pizza margherita', type: 0, price: 6, color: '#8c0032'},
+    {name: 'pizza nduja', type: 0, price: 7, color: '#002171'},
+    {name: 'pizza burrata', type: 0, price: 9, color: '#005b4f'},
+    {name: 'pizza margherita + salsiccia', type: 0, price: 7, color: '#005b9f'},
+    {name: 'kebab', type: 1, price: 6, color: '#255d00'},
     {name: 'other', type: 1, price: 0}
 ];
