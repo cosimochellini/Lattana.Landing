64cb21df8288661340f5239564773d85802c95d5
Cosimo <cosimo.chellini@gmail.com>
2019-03-08 17:38:35 +0100

    possible fix

--- js/cdn/document.js
+++ js/cdn/document.js
@@ -36,7 +36,7 @@ let getCurrentPrenotazione = async () => {
     });
 };
 
-let openReservePanuozzo = async () => {
+let openReservePanuozzo =  () => {
     let currentUser = new User();
     let $alertReservePanuozzo = $("#alertReservePanuozzo");
     let $formServePanuozzo = $("#formServePanuozzo");
